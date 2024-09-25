3c2b9ce0c2205be1de7822768aaea85db9f90c2d
reydelmeguin <ereveloa2200@alumno.ipn.mx>
2024-09-24 18:06:06 -0600

    CORRECCION

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-alert("hola , clase 5iv7 desde el js externo");
+//CLASE 24/09/2024
+//NOMBRES: REVELO ACEVEDO EMILIANO Y YAÑEZ SILVA VICTOR
+//VARIABLES 
+
+
+/*
+LAS VARIABLES GUARDAN INFORMACION QUE PUEDES REUTILIZAR
+*/
+
+//DECLARACION DE VARIABLES
+
+let nombre = 'Fornik'; //let declara varianles que puden cmabiar de valor
+const PI= 3.1416;
+var edad=25;
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a + b; //7
+let resta = a - b;
+let multiplicacion = a * b; //10
+let division = a/b; //2.5
+
+//operadores de comparacion
+
+a > b ; // true
+a < b; //false
+a == b; // false
+a === b ; // false (compara valor y tipo)
+
+
