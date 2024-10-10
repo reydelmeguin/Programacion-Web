a78e97771a2fe009a1015f7fa0e72c68c6d34645
reydelmeguin <160188069+reydelmeguin@users.noreply.github.com>
2024-10-10 16:15:42 -0600

    Add files via upload

---
+++ clase_26092024/script.js
@@ -0,0 +1,22 @@
+
+function apppendToDisplay(value) {
+    document.getElementById('display').value += value;
+    }
+
+function clearDisplay() {
+document.getElementById( 'display'). value = '';
+}
+
+function deleteLast() {
+let displayValue = document.getElementById('display').value;
+ document.getElementById('display').value = displayValue.slice(0. -1);
+}
+
+function calculateResult() {
+ try {
+    let result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+ } catch (error) {
+    alert('Expresion Invalida');
+ }
+ }
