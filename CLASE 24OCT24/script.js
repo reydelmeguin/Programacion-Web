ba826608834551e542c0a9611eaac4d91c58c37d
reydelmeguin <160188069+reydelmeguin@users.noreply.github.com>
2024-10-24 16:38:20 -0600

    Add files via upload

---
+++ CLASE 24OCT24/script.js
@@ -0,0 +1,39 @@
+//arreglos 
+/*
+let array = ["dato1", 1, Boolean];
+console.log("tamaño array: " + array.length); 
+array.push("insert nuevo val"); console.log(array[3]); 
+console.log("nuevo tamaño array: " + array.length); 
+console.log(array[3]);
+*/
+
+/*
+//PRACTICA
+console.log("TABLA DE MULTIPLICAR DEL 5:" );
+let arraycinco = [];
+
+for(i = 0;i <= 10; i++) {
+    let multi = i * 5;
+    arraycinco.push(multi);
+    console.log("5 " + "x " + i + " = " + arraycinco[i]);
+    
+    if(arraycinco.length >= 7){
+        console.log("Fin del Arreglo")
+        break;
+    }
+
+};*/
+
+/*
+//ELIMINAR ELEMENTOS DE UNA ARRAY
+let array = ["dato1","dato2","dato3"];
+array.push("dato4");
+console.log(array[3]);
+
+//ELIMINAR
+array.splice(1,1);
+console.log(array);
+*/
+
+//Lista de Tareas - Practica Elininar de un array
+
